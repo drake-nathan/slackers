06d0c3351146cc382f6a7fcc38b4318568333b45
Michael Grumbine <mgrumbine3@gmail.com>
2022-03-28 14:00:01 -0700

    missed some changes from the previous commit

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const http = require('http');
 const socketIO = require('socket.io');
+const { disconnect } = require('process');
 
 const app = express();
 
@@ -16,5 +17,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 const port = process.env.PORT || 8000;
 const server = http.createServer(app);
 // socket.io gets attached here
+// the socket represents the connection between the server and a particular client
+const io = socketIO(server);
+// attach socket listeners here
+io.on('connection', (socket) => {
+  console.log(socket.id);
+  socket.on('disconnect', () => {
+    console.log('User disconnected ', socket.id);
+  });
+});
 server.listen(port);
 console.log('Server listening on:', port);
