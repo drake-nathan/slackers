25b33e5763b69adf5556bab47f0ff78d0a4902b8
Michael Grumbine <mgrumbine3@gmail.com>
2022-03-28 13:48:11 -0700

    installed socket.io and added it to the server. Attached test listeners

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const http = require('http');
+const socketIO = require('socket.io');
 
 const app = express();
 
@@ -9,8 +10,11 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// app routes will go here
+
 // Server Setup
 const port = process.env.PORT || 8000;
 const server = http.createServer(app);
+// socket.io gets attached here
 server.listen(port);
 console.log('Server listening on:', port);
