234c6970907b8cc4a841435dddd2538d13d2a824
aelizabethian <amanda.richardson2@gmail.com>
2022-03-29 22:17:29 -0400

    login-page initial

---
+++ frontend/src/globalStyles.js
@@ -0,0 +1,13 @@
+// globalStyles.js
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    color: #1e1926;
+    margin: 0;
+    padding: 0;
+    font-family:'Montserrat', sans-serif;
+  }
+`;
+
+export default GlobalStyle;
