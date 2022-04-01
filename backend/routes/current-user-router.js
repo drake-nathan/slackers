f167248fde42a8d2792959c6e542763fd6862e79
Michael Grumbine <mgrumbine3@gmail.com>
2022-03-31 23:18:23 -0700

    endpoint works is fully functionalgit add .

--- backend/routes/current-user-router.js
+++ backend/routes/current-user-router.js
@@ -5,8 +5,8 @@ const signInRoutes = require('./sign-in');
 // all routes here are going to /api/me
 router.route('/').get(signInRoutes.currentUser);
 
-router.route('/channels').get(db.getConversationMessages);
+router.route('/channels').get(db.getUserChannels);
 
-router.route('/dms').get(db.getChannelUsers);
+router.route('/dms').get(db.getUserDms);
 
 module.exports = router;
