256ff7cae0e58386c9051657e080208a49123226
aelizabethian <amanda.richardson2@gmail.com>
2022-04-05 17:02:08 -0400

    worked on profile pics

---
+++ frontend/src/components/user-components/ProfileModal.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function ProfileModal() {
+  return (
+    <Modal>ProfileModal</Modal>
+  )
+}
+
+const Modal = styled.div`
+  position: absolute:
+  top: 40%;
+  left: 40%;
+  background: black;
+`
+
+export default ProfileModal
