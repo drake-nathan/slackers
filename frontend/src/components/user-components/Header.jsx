4e930f3d04be65d7b015b919885d8ebf87ec4722
aelizabethian <amanda.richardson2@gmail.com>
2022-04-07 09:20:37 -0400

    set up of search and fix of profile pic

--- frontend/src/components/user-components/Header.jsx
+++ frontend/src/components/user-components/Header.jsx
@@ -67,28 +67,6 @@ const UserContainer = styled.div`
   right: 0;
 `;
 
-const SearchContainer = styled.div`
-  min-width: 400px;
-  margin-left: 16px;
-  margin-right: 16px;
-`;
-
-const Search = styled.div`
-  box-shadow: inset 0 0 0 1px rgba(250, 250, 250, 0.4);
-  border-radius: 6px;
-  width: 100%;
-
-  input {
-    background: transparent;
-    border: none;
-    padding-left: 8px;
-    padding-right: 8px;
-    padding-top: 4px;
-    padding-bottom: 4px;
-    color: white;
-    outline: none;
-  }
-`;
 
 const Name = styled.div`
   padding-right: 16px;
