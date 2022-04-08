cee3f93089194d93f367b7c204c25f9ffcd5504e
dougcwest <93220603+dougcwest@users.noreply.github.com>
2022-04-08 10:55:56 -0500

    chatHeader update

--- frontend/src/components/user-components/Chat/ChatHeaderButtons.jsx
+++ frontend/src/components/user-components/Chat/ChatHeaderButtons.jsx
@@ -5,6 +5,8 @@ import axios from 'axios';
 import PropTypes from 'prop-types';
 import AddBox from '@material-ui/icons/AddBox';
 import AddIcon from '@material-ui/icons/Add';
+import PeopleIcon from '@material-ui/icons/People';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import GlobalStyles from '../../../globalStyles';
 
 import { getNonConvoUsers, addChannelUser } from '../../../context/actions';
@@ -86,9 +88,11 @@ const ChatHeaderButtons = ({ getPics, getChannels }) => {
   return (
     <ButtonDiv>
       <GlobalStyles />
-      <Button onClick={handleLeaveClick}>Leave</Button>
-      <Button onClick={handleAddUserClick}>
-        <AddIcon /> Users
+      <Button title="Add People to Channel" onClick={handleAddUserClick}>
+        <AddIcon /> <PeopleIcon />
+      </Button>
+      <Button title="Leave Channel" onClick={handleLeaveClick}>
+        <ExitToAppIcon />
       </Button>
       {modal2 && nonUsers.length > 0 && (
         <Modal>
@@ -179,15 +183,17 @@ const ButtonDiv = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  margin-right: 25px;
 `;
 
 const Button = styled.button`
   color: white;
   background: #f7969e;
   border-radius: 4px;
-  width: 80px;
+  width: 60px;
   height: 32px;
   display: flex;
+  padding: 2px;
   font-size: 1rem;
   font-weight: 500;
   align-items: center;
