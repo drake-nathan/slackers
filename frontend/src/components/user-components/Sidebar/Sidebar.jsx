35e6f4fcbd45f7ce34e3b35993687e1acb8498bd
Nathan Drake <nathandrakedrums@gmail.com>
2022-04-08 10:11:51 -0500

    fix channel list sidebar

--- frontend/src/components/user-components/Sidebar/Sidebar.jsx
+++ frontend/src/components/user-components/Sidebar/Sidebar.jsx
@@ -91,6 +91,15 @@ const Sidebar = ({ dms, setDms, channels, setChannels }) => {
               dms={dms}
             />
           ) : null}
+          <WorkSpaceContainerBottom>
+            <Send style={{ marginRight: '8px' }} />
+            <h3>
+              <strong>DMs</strong>
+            </h3>
+            <AddButton onClick={handleAddDmClick}>
+              <AddIcon />
+            </AddButton>
+          </WorkSpaceContainerBottom>
           <ChannelsList>
             {dms.map((dm, i) => (
               <Channel
@@ -102,15 +111,6 @@ const Sidebar = ({ dms, setDms, channels, setChannels }) => {
               </Channel>
             ))}
           </ChannelsList>
-          <WorkSpaceContainerBottom>
-            <Send style={{ marginRight: '8px' }} />
-            <h3>
-              <strong>DMs</strong>
-            </h3>
-            <AddButton onClick={handleAddDmClick}>
-              <AddIcon />
-            </AddButton>
-          </WorkSpaceContainerBottom>
         </ChannelsContainer>
       </InnerContainer>
     </Container>
